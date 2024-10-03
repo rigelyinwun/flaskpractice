42b00e3c4d01ddefab3ce35044a03fe401821cc7
rigelyinwun <128569895+rigelyinwun@users.noreply.github.com>
2024-10-03 13:10:53 +0800

    hahahaha

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, make_response, redirect, url_for
+from flask import Flask, render_template, request, make_response, redirect, url_for,send_from_directory,Response
+import pandas as pd
+import os
+import uuid
 
 app = Flask(__name__, template_folder='templates')
 
@@ -14,9 +17,41 @@ def index():
         else:
             return 'fail'
         
-@app.route('/file_upload')
+@app.route('/file_upload',methods=['POST'])
 def file_upload():
-    return ""
+    file=request.files['file']
+    if file.content_type=='text/plain':
+        return file.read().decode()
+    elif file.content_type=='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file.content_type=='application/vnd.ms-excel':
+        df=pd.read_excel(file)
+        return df.to_html()
+    
+@app.route('/convert_csv',methods=['POST'])
+def convert_csv():
+    file=request.files['file']
+    df=pd.read_excel(file)
+    response=Response(
+        df.to_csv(),
+        mimetype='text/csv',
+        headers={
+            'Content-Disposition':'attachment;filename=result.csv'
+        }
+    )
+    return response
+
+@app.route('/convert_csv_two',methods=['POST'])
+def convert_csv_two():
+    file=request.files['file']
+    df=pd.read_excel(file)
+    if not os.path.exists('downloads'):
+        os.makedirs('downloads')
+    filename=f'{uuid.uuid4()}.csv'
+    df.to_csv(os.path.join('downloads',filename))
+    return render_template('download.html',filename=filename)
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory('downloads',filename,download_name='result.csv')
 
 @app.route('/other')
 def other():
