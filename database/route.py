7452984d8c628374df68b6982ce57cdefeefcd72
rigelyinwun <128569895+rigelyinwun@users.noreply.github.com>
2024-10-03 17:51:55 +0800

    run

--- database/route.py
+++ database/route.py
@@ -2,7 +2,19 @@ from flask import render_template,request
 from models import Person
 
 def register_route(app,db):
-    @app.route('/')
+    @app.route('/', methods=['GET','POST'])
     def index():
-        people=Person.query.all()
-        return str(people)
+        if request.method=='GET':
+            people=Person.query.all()
+            return render_template('index.html',people=people)
+        elif request.method=='POST':
+            name=request.form.get('name')
+            age=int(request.form.get('age'))
+            job=request.form.get('job')
+            person=Person(name=name,age=age,job=job)
+            
+            db.session.add(person)
+            db.session.commit()
+
+            people=Person.query.all()
+            return render_template('index.html',people=people)
