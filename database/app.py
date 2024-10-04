3fb324f32dc764534dcd74d83f1506fc68d02d5a
rigelyinwun <128569895+rigelyinwun@users.noreply.github.com>
2024-10-04 17:36:44 +0800

    complete database

--- database/app.py
+++ database/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import psycopg2
+
+conn = psycopg2.connect("dbname=flasktutorialdb user=postgres password=ali1234 host=localhost")
+
 
 db=SQLAlchemy()
 
 def create_app():
     app = Flask(__name__,template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///./testdb.db'
+    #app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:ali1234@localhost:5432/flasktutorialdb'
     db.init_app(app)
 
     from route import register_route
